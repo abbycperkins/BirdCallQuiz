3942a7dda09f7612d4eb401658455d51aee83ba8
abbycperkins <a.catherine.perkins@gmail.com>
2023-07-20 15:09:50 -0500

    Working File - widget only

--- Widget.py
+++ Widget.py
@@ -1,134 +1,63 @@
-import contextlib
-with contextlib.redirect_stdout(None):
-    from pygame import mixer
-import re
-import requests
-import random
-import pathlib
-from bs4 import BeautifulSoup
-from PyQt5.QtWidgets import *
-from PyQt5 import QtCore, QtGui
-from PyQt5.QtGui import *
-from PyQt5.QtCore import *
-from PyQt5 import QtCore
-from PyQt5 import QtGui
-from PyQt5.Qt import Qt
-import sys
+#!/usr/bin/env python3
+
+# FIX: Read PEP8 for how imports should be structured (I fixed this already)
+
 import csv
+import pathlib
+import sys
+from collections import defaultdict
+
+# FIX: Star imports are the worst, you should explicitly call out what you want (I fixed this already)
+
+from PyQt5.Qt import Qt
+from PyQt5 import QtCore
+from PyQt5.QtWidgets import QWidget, QMainWindow, QApplication, QVBoxLayout, QTreeWidget, QTreeWidgetItem, QPushButton
+
+BIRD_CSV = pathlib.Path('~/Downloads/families_and_species.csv').expanduser()
 
 
-USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 ' \
-             'Safari/537.36'
-BASE_URL = 'https://www.bird-sounds.net/'
-parent_directory = pathlib.Path('C:/Users/Abby/PycharmProjects/BirdCallQuiz')
-directory = "Audio_Files"
-DIRECTORY = parent_directory / directory
-DIRECTORY.mkdir(parents=True, exist_ok=True)
-BIRDS = []
-data_array = []
-with open(parent_directory / 'families and species.csv') as csvfile:
-    csv_reader = csv.reader(csvfile, delimiter=",")
-    next(csv_reader, None)
-    for row in csv_reader:
-        data_array.append({'family': row[0], 'species': row[1]})
-families = []
-for fam in data_array:
-    if fam['family'] not in families:
-        families.append(fam['family'])
-
-
-class Window(QWidget):
+class BirdQuiz(QMainWindow):
     def __init__(self):
         super().__init__()
-        tw = QTreeWidget()
-        tw.itemChanged[QTreeWidgetItem,int].connect(self.change_list)
-        grid = QGridLayout(self)
-        grid.addWidget(tw)
-        self.add_birds(tw)
+        self.setWindowTitle("Bird Quiz")
 
-    def new_item(self, text):
-        item = QTreeWidgetItem()
-        item.setText(0, text)
-        item.setCheckState(0, Qt.Unchecked)
-        return item
+        layout = QVBoxLayout()
+        tree = self.create_tree()
+        layout.addWidget(tree)
 
-    def add_birds(self, p):
-        for family in families:
-            item = self.new_item(family)
-            if isinstance(p, QTreeWidget):
-                p.addTopLevelItem(item)
-            item.setFlags(item.flags() | Qt.ItemIsTristate | Qt.ItemIsUserCheckable)
-            for spec in data_array:
-                if spec['family'] == family:
-                    child = self.new_item(spec['species'])
-                    child.setFlags(child.flags() | Qt.ItemIsUserCheckable)
+        button = QPushButton('Close')
+        button.pressed.connect(self.close)
+        layout.addWidget(button)
 
-    def change_list(self, item):
-        if item.checkState() == Qt.Checked:
-            BIRDS.append(item)
-        if item.checkState() == Qt.Unchecked:
-            BIRDS.remove(item)
+        central_widget = QWidget()
+        central_widget.setLayout(layout)
+        self.setCentralWidget(central_widget)
+
+    def create_tree(self):
+        tree = QTreeWidget()
+        tree.setColumnCount(1)
+
+        data_array = defaultdict(list)
+        with BIRD_CSV.open(newline='') as csvfile:
+            for row in csv.reader(csvfile, delimiter=","):
+                data_array[row[0]].append(row[1])
+
+        for family_name, species_list in data_array.items():
+            family = QTreeWidgetItem(tree)
+            family.setFlags(family.flags() | Qt.ItemIsTristate | Qt.ItemIsUserCheckable)
+            family.setText(0, family_name)
+
+            for species_name in species_list:
+                species = QTreeWidgetItem(family)
+                species.setFlags(species.flags() | Qt.ItemIsUserCheckable)
+                species.setCheckState(0, QtCore.Qt.Unchecked)  # Checkboxes wont show up until you set a checkState
+                species.setText(0, species_name)
+
+        return tree
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    gui = Window()
-    gui.show()
-    app.exec()
-
-
-# BIRDS = input('Enter your desired bird list, separated by commas: ').split(", ")
-print(BIRDS)
-
-# page = requests.get(url)
-
-for bird in BIRDS:
-    species_url = bird.replace(' ', '-').lower()
-    url = f'{BASE_URL}{species_url}'
-    file_name = f'{species_url}.mp3'
-    if (DIRECTORY / file_name).is_file():
-        continue
-    print(f'Requesting: {url}')
-    r = requests.get(
-        url=url,
-        headers={'User-Agent': USER_AGENT},
-    )
-    r.raise_for_status()
-    soup = BeautifulSoup(r.text, 'html.parser')
-
-    for a in soup.find_all(name='source', src=re.compile(r'.*\.mp3')):
-        mp3_file = requests.get(
-            url=f'{BASE_URL}{a["src"]}',
-            headers={'User-Agent': USER_AGENT},
-        )
-
-        with open((DIRECTORY / file_name), 'wb') as f:
-            f.write(mp3_file.content)
-
-# run quiz until no more birds
-score = 0
-BIRDS_unchanged = BIRDS.copy()
-random.shuffle(BIRDS)
-for bird in BIRDS:
-    cb_sterile = bird.replace(' ', '-').lower()
-    cb_file = f'{cb_sterile}.mp3'
-
-    # play sound until player gives answer
-    mixer.init()
-    mixer.music.load(DIRECTORY / cb_file)
-    mixer.music.play(-1)
-    # time.sleep(5)
-
-    # ask player for answer
-    player_answer = input('The bird is a: ')
-    player_answer = player_answer.replace("'", "")
-    mixer.music.stop()
-
-    if player_answer.lower() == bird.lower():
-        print("Correct!")
-        score = score + 1
-    else:
-        print(f'Incorrect. The bird is a {bird}.')
-
-else:
-    print(f'You have completed your bird list! Your score was {score}/{len(BIRDS_unchanged)}')
+    main = BirdQuiz()
+    main.show()
+    sys.exit(app.exec_())
