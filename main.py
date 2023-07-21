a75619aa1ecec6a89e40e1462f3edddca27a46e0
abbycperkins <a.catherine.perkins@gmail.com>
2023-07-21 10:21:13 -0500

    Quiz works - simple

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
+import csv
+import re
+import pathlib
+import sys
+import requests
+import random
+from bs4 import BeautifulSoup
 import contextlib
 with contextlib.redirect_stdout(None):
     from pygame import mixer
-import re
-import requests
-import random
-import pathlib
-from bs4 import BeautifulSoup
-from PyQt5.QtWidgets import *
-from PyQt5.QtGui import *
-from PyQt5.QtCore import *
+from collections import defaultdict
 from PyQt5.Qt import Qt
-import sys
-
+from PyQt5 import QtCore
+from PyQt5.QtWidgets import QWidget, QMainWindow, QApplication, QVBoxLayout, QTreeWidget, QTreeWidgetItem, QPushButton, QInputDialog, QLabel, QMessageBox
 
 USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 ' \
              'Safari/537.36'
@@ -20,102 +20,126 @@ parent_directory = pathlib.Path('C:/Users/Abby/PycharmProjects/BirdCallQuiz')
 directory = "Audio_Files"
 DIRECTORY = parent_directory / directory
 DIRECTORY.mkdir(parents=True, exist_ok=True)
+BIRD_CSV = pathlib.Path(pathlib.Path('C:/Users/Abby/PycharmProjects/BirdCallQuiz/families and species.csv')).expanduser()
 BIRDS = []
 
 
-def bird_choice_window():
-    app = QApplication(sys.argv)
-    tree = QTreeWidget()
-    header_item = QTreeWidgetItem()
-    item = QTreeWidgetItem()
-    import csv
-    data_array = []
-    output = []
-    with open(parent_directory / 'families and species.csv') as csvfile:
-        csv_reader = csv.reader(csvfile, delimiter=",")
-        next(csv_reader, None)
-        for row in csv_reader:
-            data_array.append({'family': row[0], 'species': row[1]})
-    # families = ['Wrens (Troglodytidae)', 'Thrushes (Turdidae)', 'Towhees and Sparrows (Passerellidae)',
-    # 'Wood-Warblers (Parulidae)']
+class BirdQuiz(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Bird Quiz")
 
-    families = []
-    for fam in data_array:
-        if fam['family'] not in families:
-            families.append(fam['family'])
+        layout = QVBoxLayout()
+        self.tree = self.create_tree()
+        layout.addWidget(self.tree)
 
-    for family in families:
-        parent = QTreeWidgetItem(tree)
-        parent.setText(0, family)
-        parent.setFlags(parent.flags() | Qt.ItemIsTristate | Qt.ItemIsUserCheckable)
-        for spec in data_array:
-            if spec['family'] == family:
-                child = QTreeWidgetItem(parent)
-                child.setFlags(child.flags() | Qt.ItemIsUserCheckable)
-                child.setText(0, spec['species'])
-                child.setCheckState(0, Qt.Unchecked)
-    tree.setWindowTitle("Abby's Bird Sounds Quiz")
+        button = QPushButton('Submit List')
+        layout.addWidget(button)
+        button.pressed.connect(lambda: self.submit_list())
+        button.pressed.connect(lambda: self.run_quiz())
 
-    tree.setHeaderHidden(True)
-    tree.show()
-    app.exec()
+        central_widget = QWidget()
+        central_widget.setLayout(layout)
+        self.setCentralWidget(central_widget)
+
+    def create_tree(self):
+        tree = QTreeWidget()
+        tree.setColumnCount(1)
+
+        data_array = defaultdict(list)
+        with BIRD_CSV.open(newline='') as csvfile:
+            for row in csv.reader(csvfile, delimiter=","):
+                data_array[row[0]].append(row[1])
+
+        for family_name, species_list in data_array.items():
+            family = QTreeWidgetItem(tree)
+            family.setFlags(family.flags() | Qt.ItemIsTristate | Qt.ItemIsUserCheckable)
+            family.setText(0, family_name)
+
+            for species_name in species_list:
+                species = QTreeWidgetItem(family)
+                species.setFlags(species.flags() | Qt.ItemIsUserCheckable)
+                species.setCheckState(0, QtCore.Qt.Unchecked)
+                species.setText(0, species_name)
+
+        tree.setHeaderHidden(True)
+        return tree
+
+    def submit_list(self):
+        root = self.tree.invisibleRootItem()
+        for index in range(root.childCount()):
+            parent = root.child(index)
+            if parent.checkState(0) == Qt.Unchecked:
+                continue
+            else:
+                for row in range(parent.childCount()):
+                    child = parent.child(row)
+                    if child.checkState(0) == Qt.Checked:
+                        BIRDS.append(child.text(0))
+        print(BIRDS)
+
+    def run_quiz(self):
+        for bird in BIRDS:
+            species_url = bird.replace(' ', '-').lower()
+            url = f'{BASE_URL}{species_url}'
+            file_name = f'{species_url}.mp3'
+            if (DIRECTORY / file_name).is_file():
+                continue
+            print(f'Requesting: {url}')
+            r = requests.get(
+                url=url,
+                headers={'User-Agent': USER_AGENT},
+            )
+            r.raise_for_status()
+            soup = BeautifulSoup(r.text, 'html.parser')
+
+            for a in soup.find_all(name='source', src=re.compile(r'.*\.mp3')):
+                mp3_file = requests.get(
+                    url=f'{BASE_URL}{a["src"]}',
+                    headers={'User-Agent': USER_AGENT},
+                )
+
+                with open((DIRECTORY / file_name), 'wb') as f:
+                    f.write(mp3_file.content)
+
+        # run quiz until no more birds
+        random.shuffle(BIRDS)
+        score = 0
+
+        for bird in BIRDS:
+            cb_sterile = bird.replace(' ', '-').lower()
+            cb_file = f'{cb_sterile}.mp3'
+            # play sound until player gives answer
+            mixer.init()
+            mixer.music.load(DIRECTORY / cb_file)
+            mixer.music.play(-1)
+            # time.sleep(5)
+
+            # ask player for answer
+            dialog = QInputDialog(self)
+            player_answer, ok = dialog.getText(self, 'Input Dialog', 'The bird is a: ')
+            player_answer = player_answer.replace("'", "")
+            if player_answer.lower() == bird.lower():
+                score = score + 1
+
+            if ok:
+                mixer.music.stop()
+                msg = QMessageBox()
+                msg.setStandardButtons(QMessageBox.Ok)
+                msg.setWindowTitle('Answer')
+                if player_answer.lower() == bird.lower():
+                    msg.setText("Correct!")
+                else:
+                    msg.setText(f'Incorrect. The bird is a {bird}.')
+                msg.exec_()
+
+        msg.setText(f'You have completed your bird list! Your score was {score}/{len(BIRDS)}')
+        msg.setWindowTitle('Quiz complete!')
+        msg.exec_()
 
 
 if __name__ == '__main__':
-    bird_choice_window()
-
-# BIRDS = input('Enter your desired bird list, separated by commas: ').split(", ")
-# print(BIRDS)
-
-# page = requests.get(url)
-
-for bird in BIRDS:
-    species_url = bird.replace(' ', '-').lower()
-    url = f'{BASE_URL}{species_url}'
-    file_name = f'{species_url}.mp3'
-    if (DIRECTORY / file_name).is_file():
-        continue
-    print(f'Requesting: {url}')
-    r = requests.get(
-        url=url,
-        headers={'User-Agent': USER_AGENT},
-    )
-    r.raise_for_status()
-    soup = BeautifulSoup(r.text, 'html.parser')
-
-    for a in soup.find_all(name='source', src=re.compile(r'.*\.mp3')):
-        mp3_file = requests.get(
-            url=f'{BASE_URL}{a["src"]}',
-            headers={'User-Agent': USER_AGENT},
-        )
-
-        with open((DIRECTORY / file_name), 'wb') as f:
-            f.write(mp3_file.content)
-
-# run quiz until no more birds
-score = 0
-BIRDS_unchanged = BIRDS.copy()
-random.shuffle(BIRDS)
-for bird in BIRDS:
-    cb_sterile = bird.replace(' ', '-').lower()
-    cb_file = f'{cb_sterile}.mp3'
-
-    # play sound until player gives answer
-    mixer.init()
-    mixer.music.load(DIRECTORY / cb_file)
-    mixer.music.play(-1)
-    # time.sleep(5)
-
-    # ask player for answer
-    player_answer = input('The bird is a: ')
-    player_answer = player_answer.replace("'", "")
-    mixer.music.stop()
-
-    if player_answer.lower() == bird.lower():
-        print("Correct!")
-        score = score + 1
-    else:
-        print(f'Incorrect. The bird is a {bird}.')
-
-else:
-    print(f'You have completed your bird list! Your score was {score}/{len(BIRDS_unchanged)}')
+    app = QApplication(sys.argv)
+    main = BirdQuiz()
+    main.show()
+    sys.exit(app.exec_())
